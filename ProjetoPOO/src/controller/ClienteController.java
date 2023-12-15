c2ff550a92ad13833155a74135ad4b43d5af3f66
Débora Fernandes C <95572299+Deboraaluna@users.noreply.github.com>
2023-12-14 21:24:09 -0300

    Update ClienteController.java
    adicionando CRUD

--- ProjetoPOO/src/controller/ClienteController.java
+++ ProjetoPOO/src/controller/ClienteController.java
@@ -5,8 +5,10 @@
  */
 package controller;
 
+import dao.ExceptionDAO;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import model.Cliente;
 
@@ -15,7 +17,7 @@ import model.Cliente;
  * @author debor
  */
 public class ClienteController {
-    public boolean cadastrarCliente(String nome, String cpf, String email, String endereco, String nascimento) throws ParseException{
+    public boolean cadastrarCliente(String nome, String cpf, String email, String endereco, String nascimento) throws ParseException, ExceptionDAO{
         if(nome ==null && nome.length() < 0 && validarCPF(cpf) && email == null && email.length() < 0 && endereco == null && endereco.length() < 0 && validarData(nascimento)){
         SimpleDateFormat formato = new SimpleDateFormat("dd/mm/yyyy");
         Date data = formato.parse(nascimento);
@@ -25,7 +27,9 @@ public class ClienteController {
         }
         return false;
     }
-    
+    public ArrayList<Cliente> listarClientes(String nome) throws ExceptionDAO{
+        return new Cliente().listarLivros(nome);
+    }
     public boolean validarCPF(String cpf){
         for(int i = 0; i < cpf.length(); i++){
            if(!Character.isDigit(cpf.charAt(i))){
@@ -47,4 +51,25 @@ public class ClienteController {
         }
       return true;
     } 
+    public boolean alterarCliente(Integer codCliente, String nome, String cpf, String email, String endereco, String nascimento) throws ParseException, ExceptionDAO{
+        if(nome ==null && nome.length() < 0 && validarCPF(cpf) && email == null && email.length() < 0 && endereco == null && endereco.length() < 0 && validarData(nascimento)){
+        SimpleDateFormat formato = new SimpleDateFormat("dd/mm/yyyy");
+        Date data = formato.parse(nascimento);
+        Cliente cliente = new Cliente(nome, cpf, email, endereco, data);
+        cliente.setCodCliente(codCliente);
+        cliente.alterarCliente(cliente);
+        return true;
+        }
+        return false;
+    }
+    public boolean apagarCliente(int codCliente) throws ExceptionDAO{
+        if(codCliente == 0){
+            return false;
+        }else{
+            Cliente cliente = new Cliente();
+            cliente.setCodCliente(codCliente);
+            cliente.apagarCliente(cliente);
+            return true;
+        } 
+    }
 }
